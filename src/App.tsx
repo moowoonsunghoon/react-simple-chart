a03103a0a7ec1ffde7462321988d948cc286dd03
문성훈 <munseonghun@munseonghun-ui-MacBookAir.local>
2020-09-20 05:49:48 +0900

    react-simple-chart first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,165 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC<{
+  x: any[];
+  yLength?: number;
+  datas: { label: string; value: number[]; color: string }[];
+}> = ({ x, yLength = 10, datas }) => {
+  const [valueVisible, setValueVisible] = useState<number | null>(null);
+
+  let maxValues = 0;
+  for (const data of datas) {
+    maxValues += Math.max(...data.value);
+  }
+  const maxValue = maxValues.toString();
+  let maxValueDigits = "1";
+  for (let i = 0; i < maxValue.length - 1; i++) {
+    maxValueDigits += "0";
+  }
+
+  let maxY = parseInt(maxValue) / parseInt(maxValueDigits);
+
+  if (maxY % 1 === 0) maxY = (maxY + 1) * parseInt(maxValueDigits);
+  else maxY = Math.ceil(maxY) * parseInt(maxValueDigits);
+
+  const y: number[] = [];
+  for (let i = 1; i <= yLength; i++) {
+    y.push((maxY / yLength) * i);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div style={{ display: "flex", flexWrap: "wrap", marginBottom: 20 }}>
+        {datas.map((data) => (
+          <div style={{ display: "flex", marginRight: 10 }}>
+            <div
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: data.color,
+                marginRight: 10,
+              }}
+            />
+            <span>{data.label}</span>
+          </div>
+        ))}
+      </div>
+      <div style={{ minHeight: "100vh", display: "flex", flex: 1 }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column-reverse",
+            flex: 0,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <span
+            style={{
+              width: "100%",
+              paddingTop: 10,
+              position: "relative",
+              top: -7,
+            }}
+          >
+            0
+          </span>
+          {y.map((v, i) => (
+            <span
+              key={i}
+              style={{
+                display: "flex",
+                height: `${100 / y.length}%`,
+                borderRight: "1px solid #000",
+                width: "100%",
+                marginRight: 10,
+              }}
+            >
+              <span style={{ position: "relative", top: -8 }}>{v}</span>
+            </span>
+          ))}
+        </div>
+        {x.map((v, index) => (
+          <div
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+              alignItems: "center",
+              width: `${100 / x.length}%`,
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column-reverse",
+                justifyContent: "flex-start",
+                width: "100%",
+                flex: 1,
+                position: "relative",
+              }}
+              onMouseOver={() => setValueVisible(index)}
+              onMouseLeave={() => setValueVisible(null)}
+            >
+              {y.map((v, i) => (
+                <div
+                  key={i}
+                  style={{
+                    display: "flex",
+                    alignItems: "flex-end",
+                    height: `${100 / y.length}%`,
+                    borderTop: "1px solid #ababab",
+                  }}
+                />
+              ))}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column-reverse",
+                  position: "absolute",
+                  height: "100%",
+                  width: "50%",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                }}
+              >
+                {datas.map((data) => (
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      background: data.color,
+                      height: `${(data.value[index] / maxY) * 100}%`,
+                    }}
+                  >
+                    {valueVisible === index && (
+                      <div
+                        style={{
+                          color: "#fff",
+                        }}
+                      >
+                        {data.value[index]}
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div
+              style={{
+                borderTop: "1px solid #000",
+                width: "100%",
+                textAlign: "center",
+                paddingTop: 10,
+              }}
+            >
+              {v}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      x={[
+        "8.1",
+        "8.2",
+        "8.3",
+        "8.4",
+        "8.5",
+        "8.6",
+        "8.7",
+        "8.8",
+        "8.9",
+        "8.10",
+        "8.11",
+        "8.12",
+      ]}
+      datas={[
+        {
+          label: "amazon(주문수)",
+          value: [20, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 130],
+          color: "red",
+        },
+        {
+          label: "ebay(주문수)",
+          value: [30, 40, 70, 40, 10, 20, 40, 10, 30, 90, 70, 60],
+          color: "blue",
+        },
+        {
+          label: "taobao(주문수)",
+          value: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120],
+          color: "green",
+        },
+      ]}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
